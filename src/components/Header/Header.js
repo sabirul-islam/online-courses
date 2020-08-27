94d78561ce2376c9c5864cbe9ad27344503e8957
sabirul-islam <sabirul.shimul@gmail.com>
2020-08-27 18:18:50 +0600

    final

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div>
             <h1 className='text-center text-primary'> Online Courses </h1>
-            <nav className='bg-success text-white py-2'>
+            <nav className='bg-success text-white py-2 mb-2'>
                 <a className='p-4 text-white text-decoration-none' href="/courses">Online Courses</a>
                 <a className='p-4 text-white text-decoration-none' href="/instructor">Our instructor</a>
                 <a className='p-4 text-white text-decoration-none'href="/review">Student's Review</a>
